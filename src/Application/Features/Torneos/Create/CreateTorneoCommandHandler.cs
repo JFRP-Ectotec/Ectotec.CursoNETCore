c692c42658a74825319a295b034907e24946622d
Juan Fernando Rivera <jfriverap@ectotec.com>
2024-04-19 17:33:52 -0600

    feat(): Agregar manejo de Injecion de Dependencias y Serilog.

--- src/Application/Features/Torneos/Create/CreateTorneoCommandHandler.cs
+++ src/Application/Features/Torneos/Create/CreateTorneoCommandHandler.cs
@@ -2,16 +2,20 @@ using Application.Repositories;
 using Domain.Entities;
 using FluentResults;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Application.Features.Torneos.Create;
 
 public sealed class CreateTorneoCommandHandler(
     ITorneoRepository torneoRepository,
-    IUnitOfWork unitOfWork
+    IUnitOfWork unitOfWork,
+    ILogger<CreateTorneoCommandHandler> logger
 ) : IRequestHandler<CreateTorneoCommand, Result<Guid>>
 {
     public async Task<Result<Guid>> Handle(CreateTorneoCommand request, CancellationToken cancellationToken)
     {
+        logger.LogInformation("LLamada a crear torneo.");
+        logger.LogDebug(request.Tournament.ToString());
         Torneo torneo = request.Tournament;
         Result llamadaAgregar = await torneoRepository.Add(torneo);
 
---
+++ src/WebApi/Program.cs
@@ -0,0 +1,72 @@
+using Application;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+{
+    // Add services to the container.
+    // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+
+    /*
+    ! IMPORTANTE: Dejar comentarizadas ambas secciones donde se agrega el MediatR para ser
+    ! usado en injección de dependencias.
+    ! La razón es que al tratar de realizar las injecciones no están aun definidas las clases
+    ! que implementan los repositorios.
+    */
+
+    // Agregar servicios de Application
+    // var assembly = typeof(Program).Assembly;
+    // builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(assembly));
+
+    // builder.Services.AddApplication()
+    // ;
+
+    // Registrar Servicio de log.
+    builder.Host.UseSerilog((context, services, configuration) => configuration
+        .ReadFrom.Configuration(context.Configuration)
+        .ReadFrom.Services(services)
+        .Enrich.FromLogContext())
+    ;
+}
+
+var app = builder.Build();
+{
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
+
+    app.UseHttpsRedirection();
+
+    app.UseSerilogRequestLogging();
+
+    var summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    app.MapGet("/weatherforecast", () =>
+    {
+        var forecast =  Enumerable.Range(1, 5).Select(index =>
+            new WeatherForecast
+            (
+                DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                Random.Shared.Next(-20, 55),
+                summaries[Random.Shared.Next(summaries.Length)]
+            ))
+            .ToArray();
+        return forecast;
+    })
+    .WithName("GetWeatherForecast")
+    .WithOpenApi();
+
+    app.Run();
+}
+
+record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
+{
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+}
